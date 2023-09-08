d566dd36385ba404d9951cffcc913e5fa9dbbf1e
Rizky Faturriza <rfaturriza@gmail.com>
2023-09-08 09:44:22 +0700

    update(mainAppBar): remove menu icon

--- lib/features/quran/presentation/screens/components/main_app_bar.dart
+++ lib/features/quran/presentation/screens/components/main_app_bar.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../../../core/utils/themes/color.dart';
 
-class MainAppBar extends StatelessWidget  implements PreferredSizeWidget  {
+class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MainAppBar({
     super.key,
     required this.onPressedMenu,
@@ -19,14 +18,6 @@ class MainAppBar extends StatelessWidget  implements PreferredSizeWidget  {
       centerTitle: false,
       backgroundColor: Colors.transparent,
       elevation: 0,
-      leading: IconButton(
-        onPressed: onPressedMenu,
-        icon: Icon(
-          FontAwesomeIcons.bars,
-          color: defaultColor.shade50,
-          size: 20,
-        ),
-      ),
       actions: [
         IconButton(
           onPressed: onPressedQibla,
