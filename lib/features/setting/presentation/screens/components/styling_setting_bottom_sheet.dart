4fe8eae5097246593b63c4b0e08f0a1a5a593aa4
Rizky Faturriza <rfaturriza@gmail.com>
2024-08-11 12:04:18 +0700

    feat(tajweed): add explanation tajweed

--- lib/features/setting/presentation/screens/components/styling_setting_bottom_sheet.dart
+++ lib/features/setting/presentation/screens/components/styling_setting_bottom_sheet.dart
@@ -1,13 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:quranku/core/constants/font_constants.dart';
 import 'package:quranku/core/utils/extension/context_ext.dart';
 import 'package:quranku/features/setting/domain/entities/last_read_reminder_mode_entity.dart';
 import 'package:quranku/generated/locale_keys.g.dart';
 
 import '../../../../../core/components/spacer.dart';
+import '../../../../quran/presentation/utils/tajweed_rule.dart';
+import '../../../../quran/presentation/utils/tajweed_word.dart';
 import '../../bloc/styling_setting/styling_setting_bloc.dart';
+import '../helper/cache_example_tajweed.dart';
 
 class StylingSettingBottomSheet extends StatelessWidget {
   final String title;
@@ -105,7 +109,35 @@ class StylingSettingBottomSheet extends StatelessWidget {
               builder: (context, state) {
                 return SwitchListTile(
                   contentPadding: EdgeInsets.zero,
-                  title: Text(LocaleKeys.coloredTajweed.tr()),
+                  title: Row(
+                    children: [
+                      Text(LocaleKeys.coloredTajweed.tr()),
+                      const HSpacer(width: 8),
+                      InkWell(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return const DialogDetailTajweed();
+                            },
+                          );
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(2),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: context.theme.primaryColor,
+                            ),
+                          ),
+                          child: const Icon(
+                            Icons.question_mark_rounded,
+                            size: 12,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                   subtitle: Text(LocaleKeys.coloredTajweedDescription.tr()),
                   value: state.isColoredTajweedEnabled,
                   onChanged: (value) {
@@ -271,6 +303,145 @@ class _DropdownArabicFonts extends StatelessWidget {
           value: state.fontFamilyArabic,
         );
       },
+    );
+  }
+}
+
+class DialogDetailTajweed extends StatelessWidget {
+  const DialogDetailTajweed({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final tajweedExampleCache = TajweedExampleCache();
+
+    return Dialog(
+      insetPadding: const EdgeInsets.all(16),
+      child: FutureBuilder<List<TajweedWord>>(
+        future: tajweedExampleCache.getExampleAyah(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child:  CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text(LocaleKeys.defaultErrorMessage.tr());
+          }
+          return Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      LocaleKeys.coloredTajweed.tr(),
+                      style: context.textTheme.titleMedium?.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.close),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    ...ListTile.divideTiles(
+                      color: context.theme.dividerColor,
+                      context: context,
+                      tiles: TajweedRule.values.map((rule) {
+                        if (TajweedRule.getExplanation(rule).isEmpty) {
+                          return const SizedBox();
+                        }
+                        return ListTile(
+                          contentPadding: EdgeInsets.zero,
+                          title: Container(
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: rule.color(context)?.withAlpha(30),
+                            ),
+                            child: Text(
+                              TajweedRule.getTitle(rule),
+                              style: context.textTheme.bodyMedium?.copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: rule.color(context),
+                              ),
+                            ),
+                          ),
+                          subtitle: Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                MarkdownBody(
+                                  data: TajweedRule.getExplanation(rule),
+                                ),
+                                const VSpacer(),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: BlocBuilder<StylingSettingBloc,
+                                      StylingSettingState>(
+                                    buildWhen: (p, c) {
+                                      return p.fontFamilyArabic !=
+                                              c.fontFamilyArabic ||
+                                          p.arabicFontSize != c.arabicFontSize;
+                                    },
+                                    builder: (context, state) {
+                                      return Text.rich(
+                                        textAlign: TextAlign.right,
+                                        TextSpan(
+                                          children: <TextSpan>[
+                                            for (final token
+                                                in snapshot.data![rule.index]
+                                                    .tokens)
+                                              TextSpan(
+                                                text: token.text,
+                                                style: context
+                                                    .textTheme.headlineSmall
+                                                    ?.copyWith(
+                                                  height: 2.5,
+                                                  fontFamily:
+                                                      state.fontFamilyArabic,
+                                                  fontSize:
+                                                      state.arabicFontSize,
+                                                  color: () {
+                                                    if (token.rule.index ==
+                                                        rule.index) {
+                                                      return token.rule
+                                                              .color(context) ??
+                                                          context
+                                                              .theme
+                                                              .colorScheme
+                                                              .onSurface;
+                                                    }
+                                                    return null;
+                                                  }(),
+                                                ),
+                                              ),
+                                          ],
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ).toList()
+                  ],
+                ),
+              )
+            ],
+          );
+        },
+      ),
     );
   }
 }
